a42cde20a7e0810e4dd15ea6de41db0716fd15ab
Nguyễn Phú Hùng <hung.phunguyen8888@gmail.com>
2025-02-13 10:11:35 +0700

    Add files via upload

---
+++ hoc_o_lop/simple_calculator.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	char c;
+	cin >> a >> c >> b;
+	switch(c)
+	{
+		case '+':
+			cout << a+b;
+			break;
+		case '-':
+			cout << a-b;
+			break;
+		case '*':
+			cout << a*b;
+			break;
+		case '/':
+			if(b!=0)
+				cout << (double)a/b;
+			else 
+				cout << "Invalid divisor";
+			break;
+		case '%':
+			if(b!=0)
+				cout << a%b;
+			else 
+				cout << "Invalid divisor";
+			break;
+		default:
+			cout << "Invalid operator";
+	}
+}
